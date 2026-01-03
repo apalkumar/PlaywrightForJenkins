470e3bff9cfddd431f3607ff916252a9b06a8b7c
apalkumar <apalkumar@gmail.com>
2026-01-03 22:29:16 +0530

    Today's first commit

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,13 +1,13 @@
-import pytest
-from utilities.excel_register import get_excel_data
-from pages.login_page import Loginpage
+# import pytest
+# from utilities.excel_register import get_excel_data
+# from pages.login_page import Loginpage
 
-test_data = get_excel_data("test_data.xlsx", "Sheet1")
+# test_data = get_excel_data("test_data.xlsx", "Sheet1")
 
-@pytest.fixture(scope='function')
-def test_login(page):    
-    data = test_data[0]
-    login = Loginpage(page)
-    login.open(data["Url"])
-    login.enter_initial_email(data["Email"])
-    return page
+# @pytest.fixture(scope='function')
+# def test_login(page):    
+    # data = test_data[0]
+    # login = Loginpage(page)
+    # login.open(data["Url"])
+    # login.enter_initial_email(data["Email"])
+    # return page
